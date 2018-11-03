55c0724f8fb0bfd89f506663986fe4cd81e2f582
Benjamin Jones <bljones36@gmail.com>
2018-11-03 19:37:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,20 @@ class Student
 
   attr_accessor :name, :grade
   attr_reader :id
+
+  def initialize
+  end
+
+  def self.create_table
+    DB.execute(    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) )
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
