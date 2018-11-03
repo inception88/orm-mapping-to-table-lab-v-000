a74d99f0953bb624a422c240a7bd03ba42c8a39e
Benjamin Jones <bljones36@gmail.com>
2018-11-03 19:37:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,8 @@ class Student
   end
 
   def self.create_table
-    DB.execute(
-    sql =  <<-SQL 
+    DB[:conn].execute(
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
